913ae6a4d34cd29b7f662568efae68cc23e8b18a
Vahe <vahehovakimyan574@gmail.com>
2023-09-14 20:06:07 +0400

    make some components responsive

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const burger = document.getElementById("burger");
 const menu = document.getElementById("menu");
 
+const welcome_first_button = document.getElementById("welcome_mobile_first_button");
+const welcome_second_button = document.getElementById("welcome_mobile_second_button");
+const welcome_left_part = document.getElementById("welcome_left_part");
+const welcome_right_part = document.getElementById("welcome_right_part");
 
 let burger_bool = false;
+
 
 function Burger(){
 
@@ -19,4 +24,22 @@ function Burger(){
     
 }
 
+// menu.addEventListener("click", Burger);
+
 // Burger();
+
+let part_slide_bool = false;
+
+function PartSlide(){
+
+    
+    part_slide_bool = !part_slide_bool;
+
+    if(part_slide_bool){
+        welcome_left_part.style.display = "none";
+        welcome_right_part.style.display = "flex";
+    }else{
+        welcome_left_part.style.display = "flex";
+        welcome_right_part.style.display = "none";
+    }
+}
